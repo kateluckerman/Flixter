adca553f33598aaa7e323d35fc5b08dc656d42d9
kateluckerman <kateluckerman@gmail.com>
2020-06-25 17:49:20 -0500

    Added activity with rating when a movie is clicked from list

---
+++ app/src/main/java/com/example/flixter/MovieDetailsActivity.java
@@ -0,0 +1,38 @@
+package com.example.flixter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.example.flixter.models.Movie;
+
+import org.parceler.Parcels;
+import org.w3c.dom.Text;
+
+public class MovieDetailsActivity extends AppCompatActivity {
+
+    Movie movie;
+    TextView tvTitle;
+    TextView tvOverview;
+    RatingBar rbVoteAverage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_details);
+
+        tvTitle = findViewById(R.id.tvTitle);
+        tvOverview = findViewById(R.id.tvOverview);
+        rbVoteAverage = findViewById(R.id.rbVoteAverage);
+        movie = (Movie) Parcels.unwrap(getIntent().getParcelableExtra(Movie.class.getSimpleName()));
+
+        tvTitle.setText(movie.getTitle());
+        tvOverview.setText(movie.getOverview());
+
+        float voteAverage = movie.getVoteAverage().floatValue();
+        rbVoteAverage.setRating(voteAverage = voteAverage > 0 ? voteAverage / 2.0f : voteAverage);
+
+    }
+}
